eda63c5c18fcb736373cc1104a276b7f7b51af9c
Dylan Beattie <dylan@dylanbeattie.net>
2014-05-23 18:01:46 +0100

    Missed files and .csproj references due to Visual Studio save weirdness.

--- src/HeyStack.Api.Server/Services/IClock.cs
+++ src/HeyStack.Api.Server/Services/IClock.cs
@@ -5,12 +5,4 @@ namespace HeyStack.Api.Server.Services {
         /// <summary>Returns the current local date and time.</summary>
         DateTime Now { get; }
     }
-
-    public class MyClock : IClock {
-        public DateTime Now { get { return DateTime.Now; } }
-    }
-
-    public class MyHost : IHost {
-        public string MachineName { get { return Environment.MachineName; } }
-    }
 }
