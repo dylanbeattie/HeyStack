f4938944552301f3e89d64b8553e2c2fc2ef4041
Dylan Beattie <dylan@dylanbeattie.net>
2014-05-25 20:19:09 +0100

    Implemented integration tests via self-hosted API

---
+++ src/HeyStack.Api.Server.IntegrationTests/Services/StatusServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using HeyStack.Api.Server.Services;
+using HeyStack.ServiceModel.Status;
+using Moq;
+using NUnit.Framework;
+using ServiceStack;
+using Shouldly;
+
+namespace HeyStack.Api.Server.IntegrationTests.Services {
+    public class StatusServiceTests {
+
+        private const string TEST_URL = "http://localhost:1337/";
+        private Mock<IHost> mockHost;
+        private Mock<IClock> mockClock;
+
+        private TestableAppHost host;
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp() {
+            mockHost = new Mock<IHost>();
+            mockClock = new Mock<IClock>();
+            host = new TestableAppHost(container => {
+                container.Register(mockHost.Object);
+                container.Register(mockClock.Object);
+            });
+            host.Init();
+            host.Start(TEST_URL);
+        }
+
+        [Test]
+        public void StatusService_Returns_HostName_Via_Json() {
+            mockHost.Setup(mock => mock.MachineName).Returns("TESTHOST");
+            var client = new JsonServiceClient(TEST_URL);
+            var status = client.Get<StatusResultDto>(new GetStatusDto());
+            status.Status.ShouldStartWith("TESTHOST");
+        }
+
+        [Test]
+        public void StatusService_Returns_HostName_Via_Xml() {
+            mockHost.Setup(mock => mock.MachineName).Returns("TESTHOST");
+            var client = new XmlServiceClient(TEST_URL);
+            var status = client.Get<StatusResultDto>(new GetStatusDto());
+            status.Status.ShouldStartWith("TESTHOST");
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown() {
+            host.Stop();
+        }
+    }
+}
---
+++ src/HeyStack.Api.Server.IntegrationTests/Services/TestableAppHost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Funq;
+using HeyStack.Api.Server.Services;
+using ServiceStack;
+
+namespace HeyStack.Api.Server.IntegrationTests.Services {
+    /// <summary>The testable app host will host the same services as our 'real' server
+    /// but with mocked dependencies injected via a different container config.</summary>
+    public class TestableAppHost : AppSelfHostBase {
+        private readonly Action<Container> configure;
+
+        public TestableAppHost(Action<Container> configure)
+            : base("HeyStack Test Service", typeof(StatusService).Assembly) {
+            this.configure = configure;
+        }
+
+        public override void Configure(Container container) {
+            configure(container);
+        }
+    }
+}
